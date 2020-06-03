7ac1b9b8e77d380057800171a3cb530daf11083f
wujy <wujy@fapiao.com>
2020-06-03 18:07:28 +0800

    user查询接口

---
+++ routes/user.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var api  = express.Router();
+var db = require("../config/sql")
+
+api.post('/getUserList', function (req, res) {
+    // 返回的是json类型的数据
+    // 温馨提示：在浏览器中输入http://localhost:5888/api/get，可以模拟请求，拿到{"method":"GET","stu":{"name":"韩非","age":32,"height":190}}说明正常
+    db.query('SELECT * FROM sys_user', (err, results, fields) => {
+        if(err){
+            res.json({
+                code: '1000',
+                message: err
+            })
+        }
+        res.json({
+            code: '0000',
+            data: results
+        })
+    })
+});
+
+api.get('/get', function (req, res) {
+    // 返回的是json类型的数据
+    // 温馨提示：在浏览器中输入http://localhost:5888/api/get，可以模拟请求，拿到{"method":"GET","stu":{"name":"韩非","age":32,"height":190}}说明正常
+    db.query('SELECT * FROM name', (err, results, fields) => {
+        if(err){
+            console.log(err);
+        }
+        res.json({
+            method: 'GET',
+            stu: results
+        })
+        console.log(results)
+    })
+});
+
+api.post('/login', function (req, res) {
+    res.json({
+        method: 'POST',
+        car: {
+            brand: 'BWM',
+            price: 1900
+        }
+    })
+});
+module.exports = api;
