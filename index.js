39cd8b66c450c6fb4d8f1f82289b4313888e86bd
wujy <wujy@fapiao.com>
2020-06-09 10:00:55 +0800

    菜单角色查询接口

--- index.js
+++ index.js
@@ -5,13 +5,48 @@
 */
 var express = require('express');
 var app = express();
+var winston =require('winston');
+var expressWinston =require('express-winston');
+
+
+// 正常请求的日志
+app.use(expressWinston.logger({
+    transports: [
+        new (winston.transports.Console)({
+            json: true,
+            colorize: true
+        }),
+        new winston.transports.File({
+            filename: 'logs/success.log'  //成功的日志记录在log/success.log
+        })
+    ]
+}));
+
 
 // 登录接口文件
 var login = require("./routes/login");
 var user = require("./routes/user")
-
+var role = require("./routes/role")
+var menu = require("./routes/menu")
 
 app.use("/api", login);
 app.use("/api", user);
+app.use("/api", role);
+app.use("/api", menu);
+
+
+//记录错误的日志信息
+app.use(expressWinston.errorLogger({
+    transports: [
+        new winston.transports.Console({
+            json: true,
+            colorize: true
+        }),
+        new winston.transports.File({
+            filename: 'logs/error.log'   //失败的日志记录在log/success.log
+        })
+    ]
+}));
+
 
 app.listen(7001)
--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,10 @@ var api  = express.Router();
 var db = require("../config/sql")
 
 api.post('/getUserList', function (req, res) {
-    // 返回的是json类型的数据
-    // 温馨提示：在浏览器中输入http://localhost:5888/api/get，可以模拟请求，拿到{"method":"GET","stu":{"name":"韩非","age":32,"height":190}}说明正常
-    db.query('SELECT * FROM sys_user', (err, results, fields) => {
+    var sql = 'SELECT * FROM sys_role WHERE realname = ?'
+    console.log(req)
+    var req_value = [req.query.realname]
+    db.query(sql, req_value, (err, results, fields) => {
         if(err){
             res.json({
                 code: '1000',
